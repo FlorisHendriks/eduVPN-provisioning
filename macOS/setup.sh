a44abe13993e8637e018a0aa9a2fe8955c9277f3
System Administrator <root@floriss-MacBook-Pro.local>
2022-04-12 15:57:19 +0200

    name_issue

--- macOS/setup.sh
+++ macOS/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Move the no.corporate.wireguard.plist file to /Library/LaunchDaemons/
+# Move the .wireguard.plist file to /Library/LaunchDaemons/
 cp wireguard.plist /Library/LaunchDaemons/
 
 # Create a wireguard directory only accessible by the administrator
@@ -10,7 +10,7 @@ mkdir -m 600 /etc/wireguard/
 cp startVPN.sh /etc/wireguard/
 
 # Change the permissions of the wireguard launch daemon
-sudo chown root:wheel /Library/LaunchDaemons/wireguard.plist
+chown root:wheel /Library/LaunchDaemons/wireguard.plist
 
 # Load and execute the LaunchDaemon
 launchctl load /Library/LaunchDaemons/wireguard.plist
--- macOS/startVPN.sh
+++ macOS/startVPN.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# while we do not have an internet connection, we do not try to establish a vpn connection 
+# while we do not have an internet connection, we do not try to establish a vpn connection
 while ! ping -c1 -W1 vpn.strategyit.nl &> /dev/null ; do
     sleep 5
 done
 
 if [ -f '/etc/wireguard/wg0.conf' ]; then
-	wg-quick up /etc/wireguard/wg0.conf
+        wg-quick up /etc/wireguard/wg0.conf
 fi
 
 if [ -f '/etc/wireguard/expiryDate.txt' ]; then
         wireguardDate=$(gdate --date=$(head /etc/wireguard/expiryDate.txt) --iso-8601)
 else
-	ggrep -P -o '[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}(\.[0-9]+)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?' /etc/wireguard/wg0.conf > /etc/wireguard/expiryDate.txt
-        wireguardDate=$(gdate --date=$(head /etc/wireguard/expiryDate.txt) --iso-8601) 
+        ggrep -P -o '[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}(\.[0-9]+)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?' /etc/wireguar$
+        wireguardDate=$(gdate --date=$(head /etc/wireguard/expiryDate.txt) --iso-8601)
 fi
 
 domainName=$(dsconfigad -show | awk '/Active Directory Domain/{print $NF}')
@@ -21,17 +21,17 @@ computerName=$(dsconfigad -show | awk '/Computer Account/{print $NF}')
 
 certificateName="${computerName%?}.$domainName"
 
-certificateDate=$(gdate --date="$(security find-certificate -c "$certificateName" -p | openssl x509 -noout -enddate | awk -F '=' '{print $NF}' )" --iso-8601)
+certificateDate=$(gdate --date="$(security find-certificate -c "$certificateName" -p | openssl x509 -noout -enddate | awk -F '=' '{print $NF}'$
 
 if ! { [ -f '/etc/wireguard/wg0.conf' ] && [ "${wireguardDate}" '>' "${certificateDate}" ]; } then
-	wg-quick down wg0
-	config=$( CURL_SSL_BACKEND=secure-transport curl --cert "$certificateName" -d "profile_id=default" -H "Accept: application/x-wireguard-profile" https://vpn.strategyit.nl/vpn-user-portal/api/v3/provision )
-	if [ -z "$config" ]; then
-		wg-quick up /etc/wireguard/wg0.conf
-	else
-		> /etc/wireguard/wg0.conf
-		echo "$config" | sudo tee -a /etc/wireguard/wg0.conf
-		ggrep -P -o '[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}(\.[0-9]+)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?' /etc/wiregua$
-		wg-quick up /etc/wireguard/wg0.conf
-	fi
+        wg-quick down wg0
+        config=$( CURL_SSL_BACKEND=secure-transport curl --cert "$certificateName" -d "profile_id=default" -H "Accept: application/x-wireguard$
+        if [ -z "$config" ]; then
+                wg-quick up /etc/wireguard/wg0.conf
+        else
+                > /etc/wireguard/wg0.conf
+                echo "$config" | sudo tee -a /etc/wireguard/wg0.conf
+                ggrep -P -o '[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}(\.[0-9]+)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?' /etc/$
+                wg-quick up /etc/wireguard/wg0.conf
+        fi
 fi

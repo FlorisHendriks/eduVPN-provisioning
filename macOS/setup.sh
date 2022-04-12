74b6ae73973326229108138c9e43d71238dd8836
floris <floris@floriss-MacBook-Pro.local>
2022-04-12 15:46:57 +0200

    changes

--- macOS/setup.sh
+++ macOS/setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Move the no.corporate.wireguard.plist file to /Library/LaunchDaemons/
-mv wireguard.plist /Library/LaunchDaemons/
+cp wireguard.plist /Library/LaunchDaemons/
 
 # Create a wireguard directory only accessible by the administrator
 mkdir -m 600 /etc/wireguard/
 
 # Move the startVPN.sh to the wireguard directory
-mv startVPN.sh /etc/wireguard/
+cp startVPN.sh /etc/wireguard/
 
 # Change the permissions of the wireguard launch daemon
 sudo chown root:wheel /Library/LaunchDaemons/wireguard.plist

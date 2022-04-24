a7c0a64e915812a767ce6efa6324e83fd35ab5ee
FlorisHendriks98 <47246332+FlorisHendriks98@users.noreply.github.com>
2022-04-24 16:34:00 +0200

    Update startVPN.sh

--- macOS/startVPN.sh
+++ macOS/startVPN.sh
@@ -12,7 +12,7 @@ fi
 if [ -f '/etc/wireguard/expiryDate.txt' ]; then
         wireguardDate=$(gdate --date=$(head /etc/wireguard/expiryDate.txt) --iso-8601)
 else
-        ggrep -P -o '[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}(\.[0-9]+)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?' /etc/wireguar$
+        ggrep -P -o '[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}(\.[0-9]+)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?' /etc/wireguard/wg0.conf > /etc/wireguard/expiryDate.txt
         wireguardDate=$(gdate --date=$(head /etc/wireguard/expiryDate.txt) --iso-8601)
 fi
 
@@ -21,17 +21,17 @@ computerName=$(dsconfigad -show | awk '/Computer Account/{print $NF}')
 
 certificateName="${computerName%?}.$domainName"
 
-certificateDate=$(gdate --date="$(security find-certificate -c "$certificateName" -p | openssl x509 -noout -enddate | awk -F '=' '{print $NF}'$
+certificateDate=$(gdate --date="$(security find-certificate -c "$certificateName" -p | openssl x509 -noout -enddate | awk -F '=' '{print $NF}' )" --iso-8601)
 
 if ! { [ -f '/etc/wireguard/wg0.conf' ] && [ "${wireguardDate}" '>' "${certificateDate}" ]; } then
         wg-quick down wg0
-        config=$( CURL_SSL_BACKEND=secure-transport curl --cert "$certificateName" -d "profile_id=default" -H "Accept: application/x-wireguard$
+        config=$( CURL_SSL_BACKEND=secure-transport curl --cert "$certificateName" -d "profile_id=default" -H "Accept: application/x-wireguard-profile" https://vpn.strategyit.nl/vpn-user-portal/api/v3/provision )
         if [ -z "$config" ]; then
                 wg-quick up /etc/wireguard/wg0.conf
         else
                 > /etc/wireguard/wg0.conf
                 echo "$config" | sudo tee -a /etc/wireguard/wg0.conf
-                ggrep -P -o '[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}(\.[0-9]+)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?' /etc/$
+                ggrep -P -o '[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}(\.[0-9]+)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?' /etc/wireguard/wg0.conf > /etc/wireguard/expiryDate.txt
                 wg-quick up /etc/wireguard/wg0.conf
         fi
 fi

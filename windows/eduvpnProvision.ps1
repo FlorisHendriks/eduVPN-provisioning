daaa4b50e7faa3010112871b4a335cf2ce6a17b7
FlorisHendriks98 <florishendriks98@gmail.com>
2022-05-11 14:51:42 +0200

    add some logging

--- windows/eduvpnProvision.ps1
+++ windows/eduvpnProvision.ps1
@@ -9,55 +9,72 @@ if((-not($s)) -or (-not($p)))
 }
 
 # We need to have an internet connection to be able to start a WireGuard connection.
-"while(-not (Test-Connection $s -Quiet -Count 1))
-{
-	Start-Sleep -s 5
-}
+"try{
+	while(-not (Test-Connection $s -Quiet -Count 1))
+	{
+		Start-Sleep -s 5
+	}
 
-`$service = Get-Service -Name 'WireGuardTunnel`$wg0' -ErrorAction SilentlyContinue
-if(`$service -ne `$null)
-{
-    Start-Service -InputObject `$service
-}
+	`$service = Get-Service -Name 'WireGuardTunnel`$wg0' -ErrorAction SilentlyContinue
+	if(`$service -ne `$null)
+	{
+		Start-Service -InputObject `$service
+	}
 
+	`$initial = `$true
 
-`$initial = `$true
+	`$certStorePath  = `"Cert:\LocalMachine\My`"
+	`$name = hostname
+	`$MachineCertificate = Get-ChildItem -Path `$certStorePath | Where-Object {`$_.Subject -like `"*`$name*`"}
 
-`$certStorePath  = `"Cert:\LocalMachine\My`"
-`$name = hostname
-`$MachineCertificate = Get-ChildItem -Path `$certStorePath | Where-Object {`$_.Subject -like `"*`$name*`"}
+	`$stopwatch =  [system.diagnostics.stopwatch]::StartNew()
 
-if(Test-Path -Path `"C:\Program Files\WireGuard\Data\wg0Expiry.txt`" -PathType Leaf)
-{
-	`$WireguardDate = Get-Content `"C:\Program Files\WireGuard\Data\wg0Expiry.txt`"
-	`$MachineCertificateDate = Get-Date -Date `$Machinecertificate.NotAfter
-	`$initial = `$false
-}
-
-if((`$WireguardDate -lt `$MachineCertificateDate) -or `$initial)
-{
-	`$service = Get-Service -Name 'WireGuardTunnel`$wg0'
-    
-    if(`$service.Status -eq 'Running'){
-        Stop-Service -InputObject `$service
+    while((`$MachineCertificate -eq `$null) -and `$timer.Elapsed.TotalMinutes -lt 60){
+        Start-Sleep -s 60
+        `$MachineCertificate = Get-ChildItem -Path `$certStorePath | Where-Object {`$_.Subject -like `"*`$name*`"}
     }
-	
-    `$postParams = @{profile_id=`"$p`"}
 
-    `$response = Invoke-WebRequest -Uri https://$s/vpn-user-portal/api/v3/provision -Headers @{'Accept' = 'application/x-wireguard-profile'} -Body `$postParams -UseBasicParsing -Certificate `$Machinecertificate -Method Post
-    
-    if(`$response.StatusCode -eq 200)
-    {
-        [System.Text.Encoding]::UTF8.GetString(`$response.Content) | Out-File -FilePath 'C:\Program Files\WireGuard\Data\wg0.conf'
+    if(`$MachineCertificate -eq `$null){
+        `"We couldn't find a machine certificate at Cert:\LocalMachine\My with the name: `$name`" | Out-File -FilePath `"C:\Program Files\WireGuard\Data\eduVPNlog.txt`"
+        exit 1
+    }
 
-		`$response.Headers.Expires | Get-Date | Out-File -FilePath 'C:\Program Files\WireGuard\Data\wg0Expiry.txt'
-        
-		Start-Process -FilePath `"C:\Windows\System32\cmd.exe`" -verb runas -ArgumentList {/c `"`"C:\Program Files\WireGuard\wireguard.exe`" /installtunnelservice `"C:\Program Files\WireGuard\Data\wg0.conf`"`"}
-    }
-    else{
-        Start-Service -InputObject `$service
-    }
-}" | Out-File -FilePath "C:\Program Files\WireGuard\Data\wireguardRenewal.ps1"
+	if(Test-Path -Path `"C:\Program Files\WireGuard\Data\wg0Expiry.txt`" -PathType Leaf)
+	{
+		`$WireguardDate = Get-Content `"C:\Program Files\WireGuard\Data\wg0Expiry.txt`"
+		`$MachineCertificateDate = Get-Date -Date `$Machinecertificate.NotAfter
+		`$initial = `$false
+	}
+
+	if((`$WireguardDate -lt `$MachineCertificateDate) -or `$initial)
+	{
+		`$service = Get-Service -Name 'WireGuardTunnel`$wg0'
+		
+		if(`$service.Status -eq 'Running'){
+			Stop-Service -InputObject `$service
+		}
+		
+		`$postParams = @{profile_id=`"$p`"}
+
+		`$response = Invoke-WebRequest -Uri https://$s/vpn-user-portal/api/v3/provision -Headers @{'Accept' = 'application/x-wireguard-profile'} -Body `$postParams -UseBasicParsing -Certificate `$Machinecertificate -Method Post
+		
+		if(`$response.StatusCode -eq 200)
+		{
+			[System.Text.Encoding]::UTF8.GetString(`$response.Content) | Out-File -FilePath 'C:\Program Files\WireGuard\Data\wg0.conf'
+
+			`$response.Headers.Expires | Get-Date | Out-File -FilePath 'C:\Program Files\WireGuard\Data\wg0Expiry.txt'
+			
+			Start-Process -FilePath `"C:\Windows\System32\cmd.exe`" -verb runas -ArgumentList {/c `"`"C:\Program Files\WireGuard\wireguard.exe`" /installtunnelservice `"C:\Program Files\WireGuard\Data\wg0.conf`"`"}
+		}
+		else{
+			Start-Service -InputObject `$service
+		}
+	}
+}
+catch{
+    `$_ | Out-File -FilePath `"C:\Program Files\WireGuard\Data\eduVPNlog.txt`"
+}
+" | Out-File -FilePath "C:\Program Files\WireGuard\Data\wireguardRenewal.ps1"
 
 #Create a scheduled task that checks whether the wireguard configuration expires faster than the machine certificate, if so it retrieves a new wireguard configuration
 $triggerDaily= New-ScheduledTaskTrigger -At 11:00pm -Daily # Specify the trigger settings
